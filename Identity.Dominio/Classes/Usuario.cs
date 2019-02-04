becc1689f449a5c794f4259d0bf715d0704bfa4d
99825171200 <99825171200@DETIC-DEV027>
2019-02-04 09:12:45 -0400

    Criacao do Cadastro de usuario e Criacao de senha

---
+++ Identity.Dominio/Classes/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Dominio.Classes
+{
+    public class Usuario : IdentityUser
+    {
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+    }
+}
+
---
+++ Identity.InfraEstrutura/Migrations/20190204023538_CpfNoUsuario.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity.InfraEstrutura.Migrations
+{
+    public partial class CpfNoUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cpf",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cpf",
+                table: "AspNetUsers");
+        }
+    }
+}
